df6884bf8bde45c0d72ed4baf453b98138ee83ad
Flaviarayol <flavia-rayol@hotmail.com>
2022-07-27 22:49:30 -0300

    first commit

---
+++ Metodo02/src/Sobrecarga/Main.java
@@ -0,0 +1,14 @@
+package Sobrecarga;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        //Quadrilátero
+        System.out.println("Exercício de Quadriláteros");
+        Quadrilatero.area(10);
+        Quadrilatero.area(10d,5d);
+        Quadrilatero.area(10,5,3);
+        Quadrilatero.area(20f,10f);
+    }
+}
